0cb51d63b1e7aabe251f7a6b7d27a9a703ad8a52
Takeshi-Sakamoto <t-sakamoto@Takeshi-SakamotonoMacBook-Air.local>
2020-07-09 21:12:33 +0900

    Refactoring MemoList

--- App.js
+++ App.js
@@ -1,23 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import BodyText from './src/elements/BodyText';
+// import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import MemoList from "./src/components/MemoList";
+import Appbar from "./src/components/Appbar";
+import CircleButton from "./src/elements/CircleButton";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <BodyText />
-      <StatusBar style="auto" />
-    </View>
-  );
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello</Text>
+//       <BodyText />
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Appbar />
+        <MemoList />
+        <CircleButton>+</CircleButton>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 78,
+    backgroundColor: "#FFFDF6",
   },
 });
---
+++ src/components/Appbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+class Appbar extends React.Component {
+    render() {
+        return (
+            <View style={styles.appbar}>
+            <Text style={styles.appbarTitle}>SATUDORA MEMOT</Text>
+          </View>
+  
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    appbar: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        height: 78,
+        paddingTop: 30,
+        backgroundColor: "#3e5faa",
+        justifyContent: "center",
+        alignItems: "center",
+        shadowColor:"#265366",
+        shadowOffset: {width: 0 , height: 0},
+        shadowOpacity:0.9,
+        shadowRadius:3,
+        zIndex:10,
+      },
+      appbarTitle: {
+        color: "#fff",
+        fontSize: 18,
+      },
+    
+});
+
+export default Appbar;
